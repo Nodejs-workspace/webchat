8f9b04976e8c8ac9a37c9493e9636d267c2be7eb
Vinay Panwar <vinaypanwar280@gmail.com>
2024-06-08 11:11:23 +0530

    FIX: instantiated the login controller.

--- src/contexts/login.ts
+++ src/contexts/login.ts
@@ -1,8 +1,14 @@
 import LoginController from "../controllers/login";
+import UserRepository from "../repositories/user";
+import LoginService from "../services/login";
+import UserService from "../services/user";
 
 export default class LoginContext {
     public static getControllerContext() {
-        const loginController = new LoginController();
+        const userRepository = new UserRepository();
+        const userService = new UserService(userRepository);
+        const loginService = new LoginService(userService);
+        const loginController = new LoginController(loginService);
         return loginController;
     }
 }
